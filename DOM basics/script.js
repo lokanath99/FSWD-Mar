4350589625b87ae629cf16af1eac3ff9a67ac459
lokanath99 <lokanath1912@gmail.com>
2025-03-20 20:13:09 +0530

    20/03/2024

---
+++ DOM basics/script.js
@@ -0,0 +1,28 @@
+// modifying/creating the existing/new elements
+// even handling
+// // creating a element
+// var para = document.createElement("p");
+// para.innerHTML = "this is a lame paragraph";
+// para.setAttribute("class", "display-3 fw-thin");
+// para.setAttribute("style", "color: blue");
+// document.body.appendChild(para);
+
+// manipulate the existing element
+// select the element
+// var header = document.getElementsByTagName("h1");
+// change the innerHTML
+// header[0].innerHTML = "Hello from javascripts"
+// selecting by the id name
+
+document.addEventListener("DOMContentLoaded", () => {
+    // var header = document.getElementById("header");
+    // console.log(header);
+    // header.innerHTML = "Hello from java scripts";
+    // header.style.color = "darkgrey";
+    // // deleting an element
+    // var para = document.getElementById("para1");
+    // document.body.removeChild(para);
+});
+function alertOnClick() {
+    alert("Hey ther this is your java script");
+}
---
+++ JS Basics/index.js
@@ -0,0 +1,4 @@
+const num = 5;
+console.log(num);
+num = 6;
+console.log(num);
