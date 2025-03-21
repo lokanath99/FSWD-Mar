f09ef6bb573d3b55e4e7b700fb724e4ea6d4c8a4
lokanath99 <lokanath1912@gmail.com>
2025-03-21 20:11:55 +0530

    231/3/25

--- JS Basics/index.js
+++ JS Basics/index.js
@@ -1,4 +1,30 @@
-const num = 5;
-console.log(num);
-num = 6;
-console.log(num);
+// var num = 5;
+// const num = 3.14;
+// let num = 10;
+// var num1 = 90;
+// data types
+// number
+let num = 3.14;
+console.log(typeof num);
+// string
+let str1 = `lok"anath's`;
+console.log(str1);
+// boolean
+// (true -> 1)
+// (false -> 0)
+var bool = false;
+console.log(typeof bool);
+//
+var notknown; // declaration
+notknown = "hi"; //defining
+console.log(notknown);
+
+var null1 = null;
+console.log(typeof null1);
+
+// object data types
+// arrays -> sequencial data types
+var arr = [155, 55, 456, 48, 568, 98, 5, 65, 5, 65];
+//          0,   1   2    3   4    5   6  7  8   9
+arr[4] = 10010;
+console.log(arr.length);
