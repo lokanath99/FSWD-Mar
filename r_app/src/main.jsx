6c57118c0b674bffc4e0a2c4ef1d7f89c6c7286b
lokanath99 <lokanath1912@gmail.com>
2025-04-08 21:12:22 +0530

    8/4/25

--- r_app/src/main.jsx
+++ r_app/src/main.jsx
@@ -1,9 +1,53 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Navbar from "./components/Navbar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// function Football(props) {
+//     return (
+//         <>
+//             <h1>Hi From Foot ball component</h1>
+//             {/* {props.ballWithMaradona && <p>Maradona is having the ball</p>} */}
+//             {/* {condition ? true : false} */}
+//             {props.ballWithMaradona ? (
+//                 <p>Maradona is having the ball</p>
+//             ) : (
+//                 <p>other playes is having the ball</p>
+//             )}
+//         </>
+//     );
+// }
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Navbar />
+        {/* <ClassDemo person={person} /> */}
+        {/* <Football ballWithMaradona={false} /> */}
     </StrictMode>
 );
+
+// var a = 56;
+// var element = <h1>This i a reach app</h1>;
+// function ClassDemo(props) {
+//     function kick(player, event) {
+//         console.log(event);
+//         alert(`${player} hits the Goal....!`);
+//     }
+
+//     return (
+//         <>
+//             <h1 className="display-5">
+//                 Hello this is from Class Demo{" "}
+//                 {props.person.name + " " + props.person.age}
+//             </h1>
+//             <button
+//                 className="btn btn-primary"
+//                 onClick={(event) => console.log(event)}
+//             >
+//                 Kick...
+//             </button>
+//         </>
+//     );
+// }
+
+// var person = {
+//     name: "talkalot",
+//     age: 96,
+// };
